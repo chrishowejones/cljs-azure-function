b4d165c892dbe0f5c71ca3d0070558074d58a6d6
Chris Howe-Jones <chris.howejones@gmail.com>
2024-09-14 18:34:32 +0100

    Another version using resolve fn on js/Promise.

--- src/azure_function/core.cljs
+++ src/azure_function/core.cljs
@@ -4,22 +4,21 @@
             ["@azure/functions" :refer [app]]))
 
 (defn format-response [name]
-  (let [name (if (= name "") "World" name)]
-   #js {:status 200
-        :body (str "Hello, " name "!")}))
+  (let [name (if (or (nil? name)
+                     (= name ""))
+               "World"
+               name)]
+    #js {:status 200
+         :body (str "Hello, " name "!")}))
 
 (defn hello [^js req ^js context]
-  (js/Promise.
-   (fn [resolve _]
-     (.log context "Hello function processed request for url" (.-url req))
-     (if-let [query-name (-> req .-query (.get "name"))]
-       (do
-         (println "Name=" query-name)
-         (resolve (format-response query-name)))
-       (go
-         (let [body-name (<p! (.then (.text req)))]
-           (println "Name from body text=" body-name)
-           (resolve (format-response body-name))))))))
+  (js/Promise.resolve
+   (-> req
+       .text
+       (.then (fn [text]
+                (if (= text "")
+                  (format-response (-> req .-query (.get "name")))
+                  (format-response text)))))))
 
 (app.http "hello" #js {:methods #js ["GET" "POST"]
                        :authLevel "anonymous"
