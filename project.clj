ba5e33e73951bb1814a692a6d6c9fba41e39486e
Chris Howe-Jones <chris.howejones@gmail.com>
2024-09-21 15:19:49 +0100

    Remove project.clj

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject azure-function-example "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/clojurescript "1.10.866"]
-                 [thheller/shadow-cljs "2.15.2"]]
-  :source-paths ["src" "test"])
-
-
-
